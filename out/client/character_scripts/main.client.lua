fcf27ce4349fa2bf5d931cd74666d1459e63db6a
Justin <justindoane.com>
2020-01-22 18:16:08 -0500

    Made fire effects better

--- out/client/character_scripts/main.client.lua
+++ out/client/character_scripts/main.client.lua
@@ -1,10 +1,10 @@
 -- Compiled with https://roblox-ts.github.io v0.3.0
--- January 22, 2020, 5:17 PM Eastern Standard Time
+-- January 22, 2020, 6:15 PM Eastern Standard Time
 
 local TS = require(game:GetService("ReplicatedStorage"):WaitForChild("rbxts_include"):WaitForChild("RuntimeLib"));
 local Net = TS.import(script, TS.getModule(script, "net").out);
 local _0 = TS.import(script, TS.getModule(script, "services"));
-local Players, UserInputService, Lighting, Workspace, ReplicatedStorage, Debris = _0.Players, _0.UserInputService, _0.Lighting, _0.Workspace, _0.ReplicatedStorage, _0.Debris;
+local Players, UserInputService, Lighting, Workspace, ReplicatedStorage, TweenService = _0.Players, _0.UserInputService, _0.Lighting, _0.Workspace, _0.ReplicatedStorage, _0.TweenService;
 local inspect = TS.import(script, TS.getModule(script, "inspect").inspect);
 local remote = Net.ClientEvent.new("movesEvent");
 UserInputService.InputBegan:Connect(function(inputObject, gameProcessedEvent)
@@ -18,16 +18,31 @@ UserInputService.InputBegan:Connect(function(inputObject, gameProcessedEvent)
 end);
 remote:Connect(function(msg, plr)
 	if msg == "sun_damage" then
+		local asset = ReplicatedStorage:FindFirstChild("Fire"):Clone();
 		while plr.Character do
 			local sun_direction = Lighting:GetSunDirection();
+			if plr.Character then
+				asset.Parent = plr.Character:FindFirstChild("HumanoidRootPart");
+			else
+				asset.Parent = nil;
+			end;
 			local root;
 			if Players.LocalPlayer.Character then
 				root = Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart");
 			else
 				root = nil;
 			end;
+			local tweenInfo = TweenInfo.new(0.5, Enum.EasingStyle.Linear, Enum.EasingDirection.Out, 0, false, 0);
+			local t = TweenService:Create(asset, tweenInfo, {
+				LightEmission = 0;
+				Rate = 0;
+			});
+			local t2 = TweenService:Create(asset, tweenInfo, {
+				LightEmission = 0.86;
+				Rate = 70;
+			});
 			if root and (root:IsA("BasePart")) then
-				local ray = Ray.new(root.Position, (sun_direction * (20)));
+				local ray = Ray.new(root.Position, (sun_direction * (50)));
 				local _2 = ray;
 				local _1;
 				if root.Parent then
@@ -37,21 +52,25 @@ remote:Connect(function(msg, plr)
 				end;
 				local part = { Workspace:FindPartOnRayWithIgnoreList(_2, _1) };
 				if part[1] then
-					print(inspect(part[1]));
+					t:Play();
+					t.Completed:Connect(function(playbackState)
+						asset.Parent = nil;
+					end);
 				else
+					t2:Play();
+					t2.Completed:Connect(function(playbackState)
+						if plr.Character then
+							asset.Parent = plr.Character:FindFirstChild("HumanoidRootPart");
+						else
+							asset.Parent = nil;
+						end;
+					end);
 					if plr == Players.LocalPlayer then
 						remote:SendToServer("sun_damage");
 					end;
-					local asset = ReplicatedStorage:FindFirstChild("Fire"):Clone();
-					if plr.Character then
-						asset.Parent = plr.Character:FindFirstChild("HumanoidRootPart");
-					else
-						asset.Parent = nil;
-					end;
-					Debris:AddItem(asset, 3);
 				end;
 			end;
-			wait(3);
+			wait(1);
 		end;
 	end;
 end);
--- src/client/character_scripts/main.client.ts
+++ src/client/character_scripts/main.client.ts
@@ -1,6 +1,9 @@
 import Net from "@rbxts/net";
-import { Players, UserInputService, Lighting, Workspace, ReplicatedStorage, Debris } from "@rbxts/services";
+import { Players, UserInputService, Lighting, Workspace, ReplicatedStorage, Debris, TweenService } from "@rbxts/services";
 import inspect = require("@rbxts/inspect");
+// import { Standard, Deceleration, OutElastic } from "@rbxts/easing-functions";
+// import Tween, { PseudoTween } from "@rbxts/tween";
+
 
 // (async function init(){
 //     let exampleClientTwo = new Net.ClientEvent("movesEvent");
@@ -23,26 +26,47 @@ UserInputService.InputBegan.Connect((inputObject, gameProcessedEvent)=>{
 
 remote.Connect((msg: string, plr: Player)=>{
     if(msg === "sun_damage"){
+        let asset = (ReplicatedStorage.FindFirstChild("Fire") as ParticleEmitter).Clone();
+
         while (plr.Character) {
             let sun_direction = Lighting.GetSunDirection();
+            asset.Parent = plr.Character ? plr.Character.FindFirstChild("HumanoidRootPart") : undefined;
             let root = Players.LocalPlayer.Character ? Players.LocalPlayer.Character.FindFirstChild("HumanoidRootPart") : undefined;
+
+            let tweenInfo = new TweenInfo(0.5,Enum.EasingStyle.Linear, Enum.EasingDirection.Out, 0, false, 0)
+            let t: Tween = TweenService.Create(asset, tweenInfo, {
+                LightEmission: 0,
+                Rate: 0
+            });
+            let t2: Tween = TweenService.Create(asset, tweenInfo, {
+                LightEmission: 0.86,
+                Rate: 70
+            });
+
+
             if(root && root.IsA("BasePart")){
-                let ray = new Ray(root.Position, sun_direction.mul(20));
+                let ray = new Ray(root.Position, sun_direction.mul(50));
                 let part = Workspace.FindPartOnRayWithIgnoreList(ray, root.Parent ? root.Parent.GetChildren() : []);
                 if(part[0]){
-                    // blocking sun
-                    print(inspect(part[0]));
+                    t.Play();
+                    t.Completed.Connect((playbackState: Enum.PlaybackState)=>{
+                        asset.Parent = undefined;
+                    })
                 }else{
                     // not blocking sun
+                    t2.Play();
+                    t2.Completed.Connect((playbackState: Enum.PlaybackState)=>{
+                        asset.Parent = plr.Character ? plr.Character.FindFirstChild("HumanoidRootPart") : undefined;
+                    })
+                    // asset.Parent = plr.Character ? plr.Character.FindFirstChild("HumanoidRootPart") : undefined;
                     if(plr === Players.LocalPlayer){
                         remote.SendToServer("sun_damage");
                     }
-                    let asset = (ReplicatedStorage.FindFirstChild("Fire") as Fire).Clone();
-                    asset.Parent = plr.Character ? plr.Character.FindFirstChild("HumanoidRootPart") : undefined;
-                    Debris.AddItem(asset, 3);
+                    
+                    
                 }
             }   
-            wait(3);
+            wait(1);
         }
     }
 });
