10e9fa70c3dd345863dc87a854bd8f9d3f8a76d1
Justin <justindoane.com>
2020-01-22 13:46:19 -0500

    removed un-needed imports

--- src/shared/races.ts
+++ src/shared/races.ts
@@ -1,11 +1,9 @@
-import compose from "@rbxts/object-composer";
-import NetServerEvent from "@rbxts/net/out/ServerEvent";
 import * as traits from "shared/traits"
-import { Workspace, Players, ReplicatedStorage, Debris } from "@rbxts/services";
 
 type RaceNames = Exclude<keyof typeof traits, "isPerson">;
 
 let map = new Map<string, Array<RaceNames>>();
+
 map.set("Vampire",["isCombatter","isCompulser","isFeeder"]);
 
 
